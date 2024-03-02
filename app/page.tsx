be2a172cdc557527728dac22edef8fef42d29295
Michael Owuor <michael.owuor@gmail.com>
2024-03-02 08:40:39 +0300

    feat: update global CSS and add sign-in button component
    This commit updates the global CSS file to include height properties for html, body, and :root elements. It also adds a new sign-in button component that allows users to sign in with different modes (modal or redirect).

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,39 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@/components/auth/sign-in-button";
+
+const font = Poppins({ subsets: ["latin"], weight: ["600"] });
 
 export default function Home() {
 	return (
-		<div>
-			<p className="font-semibold">stuff</p>
+		<main className="flex h-full flex-col items-center justify-center bg-sky-800">
+			<div className="space-y-5 text-center">
+				<h1
+					className={cn(
+						"text-6xl font-semibold text-white drop-shadow-md",
+						font.className
+					)}
+				>
+					🔐 Auth
+				</h1>
 
-			<Button size="lg">Click Me</Button>
-		</div>
+				<p className="text-white text-lg">Simple Authentication Service</p>
+
+				<div>
+					<SignInButton>
+						<Button
+							variant="secondary"
+							size="lg"
+							className="uppercase font-semibold"
+						>
+							Sign In
+						</Button>
+					</SignInButton>
+				</div>
+			</div>
+		</main>
 	);
 }
---
+++ components/auth/sign-in-button.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ReactNode } from "react";
+import { useRouter } from "next/navigation";
+
+type SignInButtonProps = {
+	children: ReactNode;
+	mode?: "modal" | "redirect";
+	asChild?: boolean;
+};
+
+export const SignInButton = ({
+	children,
+	mode = "redirect",
+	asChild,
+}: SignInButtonProps) => {
+	const router = useRouter();
+
+	const onClick = () => {
+		router.push("/auth/sign-in");
+	};
+
+	if (mode === "modal") {
+		return <span>TODO: Implement Modal</span>;
+	}
+
+	return (
+		<span onClick={onClick} className="cursor-pointer">
+			{children}
+		</span>
+	);
+};
