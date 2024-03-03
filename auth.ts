77edd90a46307f83681c4748cb6822d58823dc1b
Michael Owuor <michael.owuor@gmail.com>
2024-03-04 01:09:14 +0300

    feat(auth): add session and jwt callbacks
    This commit adds session and jwt callbacks to the authentication module. The session callback updates the user's id and role based on the token, while the jwt callback retrieves the existing user's role from the database.

--- auth.ts
+++ auth.ts
@@ -2,9 +2,9 @@ import NextAuth from "next-auth";
 import { UserRole } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
-import authConfig from "@/auth.config";
-
 import { db } from "@/lib/db";
+import { getUserById } from "@/lib/data/user";
+import authConfig from "@/auth.config";
 
 export const {
 	auth,
@@ -12,6 +12,31 @@ export const {
 	signIn,
 	signOut,
 } = NextAuth({
+	callbacks: {
+		async session({ token, session }) {
+			if (token.sub && session.user) {
+				session.user.id = token.sub;
+			}
+
+			if (token.role && session.user) {
+				session.user.role = token.role as UserRole;
+			}
+
+			return session;
+		},
+
+		async jwt({ token }) {
+			if (!token.sub) return token;
+
+			const existingUser = await getUserById(token.sub);
+
+			if (!existingUser) return token;
+
+			token.role = existingUser.role;
+
+			return token;
+		},
+	},
 	adapter: PrismaAdapter(db),
 	session: { strategy: "jwt" },
 	...authConfig,
